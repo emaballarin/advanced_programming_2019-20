2c77da6a4f09d2b29a801ffa9bdab2764598358b
Emanuele Ballarin <emanuele@ballarin.cc>
2019-10-12 00:37:49 +0200

    Add exercises (03_erasieve.cc empty)

---
+++ exercises/c++/02_arrays/01_reverse_order.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+
+template <typename TName>
+TName *valloc(size_t vdim);
+
+template <typename TName>
+void revorder(TName inarray, size_t vdim);
+
+
+template <typename TName>
+TName *valloc(size_t vdim)
+{
+    return (new TName[vdim]);
+}
+
+template <typename TName>
+void printrev(TName *inarray, size_t vdim)
+{
+    for (size_t elem = vdim; 1; elem--)
+    {
+        cout << inarray[elem - 1] << " ";
+        if (elem == 1) break;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    size_t getvdim;
+
+    cout << "Insert the dimension if the array... " << endl;
+    cin >> getvdim;
+
+    int *intarray = valloc<int>(getvdim);
+    double *dblarray = valloc<double>(getvdim);
+
+    cout << "Insert the elements of the array..." << endl;
+
+    for (size_t getelem = 0; getelem < getvdim; getelem++)
+    {
+        cin >> dblarray[getelem];
+        intarray[getelem] = static_cast<int>(dblarray[getelem]);
+    }
+
+    printrev(dblarray, getvdim);
+    printrev(intarray, getvdim);
+
+    delete[] intarray;
+    delete[] dblarray;
+
+    return 0;
+}
---
+++ exercises/c++/02_arrays/02_isprime.cc
@@ -0,0 +1,59 @@
+// INCLUDES
+#include <cmath>
+#include <iostream>
+
+// DEFINES
+#define usl unsigned long int
+
+// NAMESPACE DECLARATIONS
+using namespace std;
+
+// PROTOTYPES
+bool isprime(usl);
+usl *replalloc(usl *, size_t, usl);
+
+// FUNCTIONS
+bool isprime(usl nin)
+{
+    for (usl i = 2; i <= floor(sqrt(nin)); i++)
+    {
+        if ((nin % i) == 0) { return 0; }
+    }
+    return 1;
+}
+
+usl *replalloc(usl *prevvec, size_t prevdim, usl newelem)
+{
+    usl *retvec = new usl[prevdim + 1];  // Allocate on the heap
+
+    // Copy vector
+    for (usl i = 0; i < prevdim; i++) { retvec[i] = prevvec[i]; }
+
+    // Deallocate previous vector
+    delete[] prevvec;
+
+    // Add element
+    retvec[prevdim] = newelem;
+
+    return retvec;
+}
+
+// MAIN
+int main()
+{
+    usl *primes = new usl[0];
+    usl nrprimes{0};
+
+    for (usl i = 2; i <= 100; i++)
+    {
+        if (isprime(i))
+        {
+            primes = replalloc(primes, nrprimes, i);
+            nrprimes++;
+        }
+    }
+
+    for (usl i = 0; i < nrprimes; i++) { cout << primes[i] << endl; }
+
+    delete[] primes;
+}
