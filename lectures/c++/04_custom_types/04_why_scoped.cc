a2167ba846e9a2f0755b3edec85f9ae5d71ddac2
Emanuele Ballarin <emanuele@ballarin.cc>
2019-10-22 06:14:15 +0200

    Reformat lecture 04

--- lectures/c++/04_custom_types/04_why_scoped.cc
+++ lectures/c++/04_custom_types/04_why_scoped.cc
@@ -1,12 +1,27 @@
 #include <iostream>
 
-enum month { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec };
+enum month
+{
+    jan = 1,
+    feb,
+    mar,
+    apr,
+    may,
+    jun,
+    jul,
+    aug,
+    sep,
+    oct,
+    nov,
+    dec
+};
 
 using namespace std;
 
-int main() {
-  cout << nov << endl;
-  // cout << dec << endl;
+int main()
+{
+    cout << nov << endl;
+    // cout << dec << endl;
 
-  return 0;
+    return 0;
 }
--- lectures/c++/04_custom_types/07_template_class.cc
+++ lectures/c++/04_custom_types/07_template_class.cc
@@ -2,54 +2,55 @@
 #include <string>
 
 template <typename num>
-class Vector {
-  num* elem;
-  std::size_t _size;
+class Vector
+{
+    num *elem;
+    std::size_t _size;
 
- public:
-  Vector(const std::size_t size) : elem{new num[size]}, _size{size} {}
+        public:
+    Vector(const std::size_t size) : elem{new num[size]}, _size{size} {}
 
-  // automatically release the acquired memory
-  ~Vector() { delete[] elem; }
+    // automatically release the acquired memory
+    ~Vector() { delete[] elem; }
 
-  // try to remove the const and recompile
-  std::size_t size() const { return _size; }
+    // try to remove the const and recompile
+    std::size_t size() const { return _size; }
 
-  num& operator[](const std::size_t i) { return elem[i]; }
+    num &operator[](const std::size_t i) { return elem[i]; }
 
-  // try to comment this line and recompile
-  const num& operator[](const std::size_t i) const { return elem[i]; }
+    // try to comment this line and recompile
+    const num &operator[](const std::size_t i) const { return elem[i]; }
 };
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const Vector<T>& v) {
-  for (auto i = 0u; i < v.size(); ++i)
-    os << "v[" << i << "] = " << v[i] << std::endl;
-  return os;
+std::ostream &operator<<(std::ostream &os, const Vector<T> &v)
+{
+    for (auto i = 0u; i < v.size(); ++i) os << "v[" << i << "] = " << v[i] << std::endl;
+    return os;
 }
 
-int main() {
-  Vector<double> v{10};
+int main()
+{
+    Vector<double> v{10};
 
-  for (auto i = 0u; i < v.size(); ++i)
-    v[i] = i;
+    for (auto i = 0u; i < v.size(); ++i) v[i] = i;
 
-  std::cout << v << std::endl;
+    std::cout << v << std::endl;
 
-  Vector<double>* pv{&v};
+    Vector<double> *pv{&v};
 
-  // first dereference the pointer, then I can use the defined operators
-  (*pv)[0] = -99.999;
+    // first dereference the pointer, then I can use the defined operators
+    (*pv)[0] = -99.999;
 
-  pv->operator[](1) = 77777.3333;  // or I call the function by name
+    pv->operator[](1) = 77777.3333;  // or I call the function by name
 
-  std::cout << *pv << std::endl;
+    std::cout << *pv << std::endl;
 
-  Vector<double>& rv{v};
+    Vector<double> &rv{v};
 
-  rv[5] = 555;
+    rv[5] = 555;
 
-  std::cout << v << std::endl;
+    std::cout << v << std::endl;
 
-  return 0;
+    return 0;
 }
