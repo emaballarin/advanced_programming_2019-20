3ac55cea93a02c978010e97410abaf0140f114aa
Emanuele Ballarin <emanuele@ballarin.cc>
2019-11-07 12:36:23 +0100

    Reformat and Update

--- lectures/c++/06_error_handling/00_errno.cc
+++ lectures/c++/06_error_handling/00_errno.cc
@@ -1,12 +1,18 @@
 #include <cmath>
 #include <iostream>
 
-int main() {
-  double d = std::sqrt(4);
-  std::cout << d << " " << errno << std::endl;
+int main()
+{
+    double d = std::sqrt(4);
+    std::cout << d << " " << errno << std::endl;
 
-  d = std::sqrt(-4);
-  std::cout << d << " " << errno << std::endl;
+    d = std::sqrt(-4);
+    std::cout << d << " " << errno << std::endl;
 
-  return 0;
+    /*
+    The `errno` is a GLOBAL variable modified by any function/"routine" and that is NOT reset to the defaukt of 0 unless redefined by the user.
+    This represents "error propagation".
+    */
+
+    return 0;
 }
--- lectures/c++/06_error_handling/05_stack_unwinding.cc
+++ lectures/c++/06_error_handling/05_stack_unwinding.cc
@@ -1,78 +1,105 @@
+#include "ap_error.h"
+
 #include <iostream>
 #include <vector>
 
-#include "ap_error.h"
-
-class Foo {
- public:
-  Foo() { std::cout << "Foo" << std::endl; }
-  ~Foo() { std::cout << "~Foo" << std::endl; }
-};
-
-class Bar {
- public:
-  Bar() { std::cout << "Bar" << std::endl; }
-  ~Bar() { std::cout << "~Bar" << std::endl; }
-};
-
-class Vector {
-  double* elem;
-
- public:
-  Vector(const unsigned int l) : elem{new double[l]} {
-    std::cout << "Vector" << std::endl;
-  }
-  ~Vector() noexcept {
-    delete[] elem;
-    std::cout << "~Vector" << std::endl;
-  }
-};
-
-class ManyResources {
-  double* ptr;
-  Vector v;
-
- public:
-  ManyResources() : ptr{nullptr}, v{3} {
-    std::cout << "Manyresources" << std::endl;
-    try {
-      ptr = new double[5];  // new(std::nothrow) double[5] could be better
-      AP_ERROR(false) << "Error in ManyResources ctor." << std::endl;
-    } catch (...) {
-      delete[] ptr;  // <----
-      throw;
+class Foo
+{
+    public:
+    Foo()
+    {
+        std::cout << "Foo" << std::endl;
     }
-  }
-
-  ~ManyResources() noexcept {
-    std::cout << "Manyresources" << std::endl;
-    delete[] ptr;  // <----
-  }
+    ~Foo()
+    {
+        std::cout << "~Foo" << std::endl;
+    }
 };
 
-int main() {
-  Foo f;
-  int* raw_ptr = new int[7];
-  try {
-    // int * raw_ptr=new int[7]; // wrong because raw_ptr would not be visible
-    // inside the catch-clause
-    ManyResources mr;
-    Bar b;
+class Bar
+{
+    public:
+    Bar()
+    {
+        std::cout << "Bar" << std::endl;
+    }
+    ~Bar()
+    {
+        std::cout << "~Bar" << std::endl;
+    }
+};
 
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
+class Vector
+{
+    double* elem;
 
-    delete[] raw_ptr;  // <--- try to comment this and run valgrind or use the
-                       // sanitize library
-    return 1;
+    public:
+    Vector(const unsigned int l) : elem{new double[l]}
+    {
+        std::cout << "Vector" << std::endl;
+    }
+    ~Vector() noexcept
+    {
+        delete[] elem;
+        std::cout << "~Vector" << std::endl;
+    }
+};
 
-  } catch (...) {
-    std::cerr << "Unknown exception. Aborting.\n" << std::endl;
+class ManyResources
+{
+    double* ptr;
+    Vector v;
+
+    public:
+    ManyResources() : ptr{nullptr}, v{3}
+    {
+        std::cout << "Manyresources" << std::endl;
+        try
+        {
+            ptr = new double[5];  // new(std::nothrow) double[5] could be better
+            AP_ERROR(false) << "Error in ManyResources ctor." << std::endl;
+        }
+        catch (...)
+        {
+            delete[] ptr;  // <----
+            throw;         // Whatever has been catched!
+        }
+    }
+
+    ~ManyResources() noexcept
+    {
+        std::cout << "Manyresources" << std::endl;
+        delete[] ptr;  // <----
+    }
+};
+
+int main()
+{
+    Foo f;
+    int* raw_ptr = new int[7];
+    try
+    {
+        // int * raw_ptr=new int[7]; // wrong because raw_ptr would not be visible
+        // inside the catch-clause
+        ManyResources mr;
+        Bar b;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+
+        delete[] raw_ptr;  // <--- try to comment this and run valgrind or use the
+                           // sanitize library
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception. Aborting.\n" << std::endl;
+
+        delete[] raw_ptr;  // <---
+        return 2;
+    }
 
     delete[] raw_ptr;  // <---
-    return 2;
-  }
-
-  delete[] raw_ptr;  // <---
-  return 0;
+    return 0;
 }
